f296e4246bd55351ea8e8cfd50d37373b11948e6
Kyle Welsby <kyle@mekyle.com>
2021-01-14 23:21:00 +0000

    🐛missing Provider Icons

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+const SIZES = [12, 32, 48, 84];
+const whitelistSizes = [];
+SIZES.forEach((size) => {
+  whitelistSizes.push(`w-${size}`);
+  whitelistSizes.push(`h-${size}`);
+  whitelistSizes.push(`min-w-${size}`);
+  whitelistSizes.push(`min-h-${size}`);
+  whitelistSizes.push(`max-w-${size}`);
+  whitelistSizes.push(`max-h-${size}`);
+});
+module.exports = {
+  theme: {
+    extend: {
+      spacing: {
+        72: '18rem',
+        84: '21rem',
+        96: '24rem',
+      }
+    }
+  },
+  purge: {
+    enabled: process.env.NODE_ENV === "production",
+    content: ["str/**/*.vue"],
+    option: {
+      safelist: [].concat(whitelistSizes)
+    }
+  },
+  future: {
+    removeDeprecatedGapUtilities: true
+  }
+};
