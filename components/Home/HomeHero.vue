1501cf5f1c76483873dea10a2eb7ecec5bb4e856
Kyle Welsby <kyle@mekyle.com>
2021-02-22 18:47:03 +0000

    ✨ Homepage hero

---
+++ components/Home/HomeHero.vue
@@ -0,0 +1,72 @@
+<template lang="pug">
+  div(
+    class="bg-white flex flex-col items-center"
+  )
+    div(
+      class="w-full md:w-10/12 p-4 py-8 flex flex-col md:flex-row items-center"
+    )
+      div(
+        class="flex-grow my-4 mb-8 md:m-0 px-4 md:px-0"
+      )
+        p(
+          class="text-4xl lg:text-5xl font-serif leading-tight mb-3"
+        )
+          | For the artists,
+          br
+          | listeners,
+          br
+          | and discovery
+          br
+          | of timeless sounds.
+        nuxt-link(
+          to="/episodes/"
+          class="font-medium text-gray-500 flex flex-row items-center"
+        ) Go to Tracklists ›
+      div(
+        class="flex-grow text-right flex flex-col items-end content-end justify-end max-w-xs"
+      )
+        h1
+          span(
+            class="block uppercase tracking-widest font-light mb-2"
+          ) New tracklist
+          span(
+            class="block font-semibold text-2xl mb-5"
+          ) &nbsp;{{ latestShow.title }}
+        nuxt-link(
+          :to="`/episodes/${latestShow.slug}/`"
+          class="flex flex-col content-center"
+        )
+          Artwork(
+            :src="latestShow.artwork"
+            class="mb-5"
+            :size="327"
+          )
+        nuxt-link(
+          :to="`/episodes/${latestShow.slug}/`"
+          @click.native="playShow()"
+          class="bg-black rounded-full text-white px-6 py-2 font-medium text-l flex flex-row self-center"
+        )
+          nuxt-img(
+            src="/icons/play.svg"
+            format="svg"
+            provider="static"
+            size="25"
+            class="w-6 mr-2"
+          )
+          | Play Show
+</template>
+<script>
+export default {
+  props: {
+    latestShow: {
+      type: Object,
+      default: () => {},
+    },
+  },
+  methods: {
+    playShow() {
+      console.info('TODO PLAY SHOW')
+    },
+  },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -2,10 +2,31 @@
   div(
     class="bg-white flex flex-col items-stretch"
   )
+    HomeHero(
+      :latest-show="latestShow"
+    )
+    //- News events
+
 </template>
 
 <script>
 export default {
+  async asyncData({ $supabase, $config, error }) {
+    const { error: err, data } = await $supabase
+      .from('shows')
+      .select('title, slug, artwork')
+      .eq('profile_id', 1)
+      .order('published_at', { ascending: false })
+      .limit(1)
+      .single()
+    if (err) {
+      console.error(err)
+      error({ statusCode: 500 })
+    }
+    return {
+      latestShow: data,
+    }
+  },
   head() {
     return {
       title: '',
