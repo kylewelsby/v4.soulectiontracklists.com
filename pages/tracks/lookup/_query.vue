752b7494b5bf440dc78e3c8e914f60120bf3b2e2
Kyle Welsby <kyle@mekyle.com>
2021-03-17 16:06:28 +0000

    ✨ Track lookup with good search

--- pages/tracks/lookup/_query.vue
+++ pages/tracks/lookup/_query.vue
@@ -6,32 +6,44 @@
     div(
       class="w-full md:w-10/12 p-4 py-8"
     )
-      //- SearchResult(
-      //-   v-for="result in data"
-      //-   :key="result.id"
-      //-   :artwork="result.artwork"
-      //-   :to="`/tracks/${result.id}/`"
-      //- ) {{ result.title }}
+      h1(
+        class="text-xl"
+      ) Search Results
+      small(
+        class="text-xm mb-4"
+      ) {{ $tc('searchResults.count', data.length) }}
+      SearchResult(
+        v-for="result in data"
+        :key="result.id"
+        :artwork="result.artwork"
+        :to="`/tracks/${result.id}/`"
+      ) {{ result.artist.title }} - {{ result.title }}
 </template>
 
 <script>
 export default {
-  async asyncData({ $supabase, query, error }) {
+  async asyncData({ $sentry, $supabase, query, error }) {
+    const searchResp = await $supabase
+      .rpc('search_tracks', { query: query.q })
+      .select('id')
+    if (searchResp.error) {
+      $sentry.captureException(searchResp.error)
+      error({ statusCode: 500 })
+      return
+    }
+    const trackIds = searchResp.data.map((r) => r.id)
     const resp = await $supabase
-      .rpc('search_tracks', { query: 'test' })
-      .select('*')
-    console.log(resp)
-    // .from('artists')
-    // .select(
-    //   'id, title, slug, tracks(id, slug, title, artwork), lower(title) as ltitle'
-    // )
-    // .limit(10)
-    // if (err) {
-    //   error({ statusCode: 500 })
-    // }
-    // return {
-    //   data,
-    // }
+      .from('tracks')
+      .select('id, title, artwork, artist(id, title)')
+      .in('id', trackIds)
+    if (resp.error) {
+      $sentry.captureException(resp.error)
+      error({ statusCode: 500 })
+    }
+
+    return {
+      data: resp.data,
+    }
   },
   watch: {
     '$route.query'() {
