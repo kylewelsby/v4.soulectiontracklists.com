dfe762ce9a61c53728b344e448e5132c4dcf9c8d
Kyle Welsby <kyle@mekyle.com>
2021-03-24 17:10:15 +0000

    🚧 Record/album pages

---
+++ components/Albums/AlbumsListItem.vue
@@ -0,0 +1,38 @@
+<template lang="pug">
+  nuxt-link(
+    to="/"
+    class="flex flex-col items-start"
+  )
+    Artwork(
+      :src="album.artwork"
+      :size="512"
+      class="shadow-lg rounded-2xl"
+    )
+    h2(
+      class="mt-4 text-2xl font-bold tracking-wide"
+    ) {{ album.title }}
+    h3(
+      class="text-1xl font-medium tracking-wide"
+    ) {{ album.artist.title }}
+    div(
+      class="mt-2 text-sm font-light whitespace-nowrap"
+    )
+      | {{ albumType }}
+      | &middot;
+      | {{ album.published_at | formattedDate }}
+</template>
+<script>
+export default {
+  props: {
+    album: {
+      type: Object,
+      default: () => {},
+    },
+  },
+  computed: {
+    albumType() {
+      return 'Album'
+    },
+  },
+}
+</script>
--- pages/records/index.vue
+++ pages/records/index.vue
@@ -1,4 +1,38 @@
 <template lang="pug">
-  div
-    | Coming Soon
+  div(
+    class="flex flex-col items-center"
+  )
+    div(
+      class="grid grid-cols-3 gap-8 px-4 w-full md:w-10/12"
+    )
+      AlbumsListItem(
+        v-for="album in albums"
+        :key="album.id"
+        :album="album"
+      )
 </template>
+<script>
+export default {
+  async asyncData({ $supabase, error }) {
+    const { data: albums, error: err } = await $supabase
+      .from('albums')
+      .select(
+        `id,
+        title,
+        artwork,
+        published_at,
+        artist(
+          id,
+          title
+        )`
+      )
+      .order('published_at', { ascending: false })
+    if (err) {
+      error({ statusCode: 500, message: err, err })
+    }
+    return {
+      albums,
+    }
+  },
+}
+</script>
