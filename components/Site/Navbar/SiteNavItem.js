7042390fae0ff5794f7c76578ad8349fe6e2c2d2
Kyle Welsby <kyle@mekyle.com>
2021-03-27 17:56:32 +0000

    ♻️ Dynamic active navbar link

--- components/Site/Navbar/SiteNavItem.js
+++ components/Site/Navbar/SiteNavItem.js
@@ -11,24 +11,43 @@ export default Vue.component('SiteNavItem', {
     },
     localAttrs() {
       if (this.tag === 'a') {
-        return Object.assign(this.$attrs, {
-          rel: 'noopener',
-          target: '_blank',
-        })
+        return Object.assign(
+          {
+            rel: 'noopener',
+            target: '_blank',
+          },
+          this.$attrs
+        )
+      } else if (this.tag === 'nuxt-link') {
+        return Object.assign({ to: '#' }, this.$attrs)
       } else {
         return this.$attrs
       }
+    },
+    activeClasses() {
+      const classes = 'text-opacity-100 font-semibold'
+      let path
+      if (this.$attrs.to) {
+        if (typeof this.$attrs.to === 'string') {
+          path = this.$attrs.to
+        } else {
+          path = this.$attrs.path
+        }
+        if (this.$route.fullPath.startsWith(path)) {
+          return classes
+        }
+      }
+      return ''
     },
   },
   render(h) {
     return h(
       this.tag,
       {
-        class:
-          'py-2 px-3 text-center text-xs text-black text-opacity-50 hover:underline hover:text-opacity-75 dark:text-white dark:text-opacity-50',
+        class: `py-2 px-3 text-center text-xs text-black text-opacity-50 hover:underline hover:text-opacity-75 active:text-opacity-100 dark:text-white dark:text-opacity-50 dark:hover:text-opacity-75 dark:active:text-opacity-100 ${this.activeClasses}`,
         on: this.$listeners,
         attrs: this.localAttrs,
-        props: this.$props,
+        props: Object.assign(this.localAttrs, this.$props),
       },
       this.$slots.default
     )
