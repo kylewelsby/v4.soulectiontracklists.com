fe2d858c1a5cbaebfbc3ef01c4dd0b0aac7606c0
Kyle Welsby <kyle@mekyle.com>
2021-05-12 19:48:30 +0100

    ✨ Contact form link to old site

---
+++ pages/contact/index.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+  div(
+    class="container mx-auto flex flex-col items-center mt-10"
+  )
+    div(
+      class="prose prose-sm sm:prose lg:prose-lg dark:prose-dark px-4 mx-auto"
+    )
+      h1(
+        class="font-header uppercase"
+      ) Contact
+    div(
+      class="my-20"
+    )
+      SiteButton(
+        href="https://soulection.squarespace.com/contact"
+      ) Visit old site contact form
+</template>
