5971b07efdfff8e0461ebedada998bd4045e0006
Kyle Welsby <kyle@mekyle.com>
2021-06-03 19:53:41 +0100

    cache clear

---
+++ cache-clear.sh
@@ -0,0 +1,3 @@
+npx layer0 cache-clear --team=kyle-welsby --site=soulection-tracklists-v4 --environment=default --path=/
+npx layer0 cache-clear --team=kyle-welsby --site=soulection-tracklists-v4 --environment=default --path=/tracklists/
+npx layer0 cache-clear --team=kyle-welsby --site=soulection-tracklists-v4 --environment=default --path=/records/
